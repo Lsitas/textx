093e3bf391283a2cabc2b48219fc4d34f4b3bf8c
lsitas <sitas.luka.97@gmail.com>
2020-02-01 14:50:56 -0500

    created the client handler

--- client.py
+++ client.py
@@ -1,14 +1,15 @@
 import socket               # Import socket module
 
 
+s = socket.socket()  # Create a socket object
+host = 'localhost'  # Get local machine name
+port = 3419  # Reserve a port for your service.
+s.connect((host, port))
 name = ''
 while name != 'q':
     name = input("Name: ")
-    s = socket.socket()  # Create a socket object
-    host = 'localhost'  # Get local machine name
-    port = 3419  # Reserve a port for your service.
-    s.connect((host, port))
     s.send(bytes(name, 'utf-8'))
     print(str(s.recv(1024),'utf-8'))
     print(str(s.recv(1024),'utf-8'))
-    s.close()
+
+s.close()
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 host = 'localhost'
 port = 3419
 name = ''
@@ -7,16 +8,17 @@ inputs = ''
 def client_handler(c, addr):
    global inputs
    print('Got connection from', addr)
-   name = c.recv(1024)
-   name = str(name, 'utf-8')
-   inputs += name
-   if name == 'q':
-      c.send(bytes("Good Bye, " + name, 'utf-8'))
-   else:
-      c.send(bytes("Thanks for connecting, " + name, 'utf-8'))
-   c.send(bytes(inputs, 'utf-8'))
-   print(inputs)
-
+   while True:
+       name = c.recv(1024)
+       name = str(name, 'utf-8')
+       inputs += name
+       if name == 'q':
+          c.send(bytes("Good Bye, " + name, 'utf-8'))
+       else:
+          c.send(bytes("Thanks for connecting, " + name, 'utf-8'))
+       c.send(bytes(inputs, 'utf-8'))
+       print(inputs)
+   c.close()
 
 
 s = socket.socket()
@@ -27,5 +29,5 @@ s.listen(5)
 print((host,port))
 while name != 'q':
    c, addr = s.accept()     # Establish connection with client.
-   client_handler(c,addr)   # set up a client handler
-c.close()
+   threading.Thread.start(client_handler(c,addr))# set up client handler
+s.close()
