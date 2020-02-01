075f70e24f843a0ce93ca95f3b941f3609726373
lsitas <sitas.luka.97@gmail.com>
2020-02-01 14:27:56 -0500

    created the client handler

--- server.py
+++ server.py
@@ -1,16 +1,11 @@
 import socket
-
-s = socket.socket()
-
 host = 'localhost'
 port = 3419
-s.bind((host,port))
-s.listen(5)
 name = ''
 inputs = ''
-print((host,port))
-while name != 'q':
-   c, addr = s.accept()     # Establish connection with client.
+
+def client_handler(c, addr):
+   global inputs
    print('Got connection from', addr)
    name = c.recv(1024)
    name = str(name, 'utf-8')
@@ -19,7 +14,18 @@ while name != 'q':
       c.send(bytes("Good Bye, " + name, 'utf-8'))
    else:
       c.send(bytes("Thanks for connecting, " + name, 'utf-8'))
-   c.send(bytes(inputs,'utf-8'))
+   c.send(bytes(inputs, 'utf-8'))
    print(inputs)
 
+
+
+s = socket.socket()
+
+s.bind((host,port))
+s.listen(5)
+
+print((host,port))
+while name != 'q':
+   c, addr = s.accept()     # Establish connection with client.
+   client_handler(c,addr)   # set up a client handler
 c.close()
