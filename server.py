211e4af6580cfddaf96ce797d2639eb624477add
lsitas <sitas.luka.97@gmail.com>
2020-02-15 15:25:12 -0500

    Working on 1 to many

--- server.py
+++ server.py
@@ -6,17 +6,26 @@ host = 'localhost'
 port = 3419
 s.bind((host,port))
 s.listen(5)
-#senders = []
-
-
 print((host,port))
+
+senders = []
+listeners = []
+
+
+
+
+
+
 while True:
-   sender, addr = s.accept()     # Establish connection with client.
-   print('Got connection from', sender, ' as Sender')
-   listener, addr = s.accept()     # Establish connection with client.
-   print('Got connection from', addr, ' as Listener')
-   while True:
-      name = sender.recv(1024)
-      listener.send(name)
-   sender.close()
-   listener.close()
+   conn, addr = s.accept()     # Establish connection with client.
+   if (conn.recv(1) == bytes(1)):
+      print('Got connection from', addr, ' as Sender')
+      senders.append(conn)
+   else:
+      print('Got connection from', addr, ' as Listener')
+      listeners.append(conn)
+
+   for i in senders:
+      i.close()
+   for i in listeners:
+      i.close()
